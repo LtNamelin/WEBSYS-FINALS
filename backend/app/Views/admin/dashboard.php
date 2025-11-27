f8c5d6c264ecf20e8db0529fb76bc942c08f3ba4
Bibingsu <vincearabebo3@gmail.com>
2025-11-27 12:22:12 +0800

    feat: update
    updated dashboard for admin
    updated moodboard

--- backend/app/Views/admin/dashboard.php
+++ backend/app/Views/admin/dashboard.php
@@ -2,7 +2,7 @@
 <html>
 <?= view('components/head', [
     'title' => 'My Coffee | Admin Dashboard'
-])?>
+]) ?>
 
 <body class="color-dark-latte">
     <!-- NavBar -->
@@ -14,10 +14,10 @@
             <?= view('components/sidebar/adminSidebar', ['active' => 'dashboard']) ?>
 
             <!-- Main Content -->
-            <div class="flex-1 mx-8 my-8 space-y-12">
+            <div class="flex-1 space-y-12 mx-8 my-8">
                 <!-- Admin Dashboard Stats -->
-                <section class="p-6 color-light-cappuccino rounded-xl shadow">
-                    <h2 class="text-2xl text-center text-color-dark-espresso font-bold mt-4 mb-8">
+                <section class="shadow p-6 rounded-xl color-light-cappuccino">
+                    <h2 class="mt-4 mb-8 font-bold text-color-dark-espresso text-2xl text-center">
                         Admin Dashboard
                     </h2>
 
@@ -26,25 +26,25 @@
                             'title' => 'Total Current Users:',
                             'value' => 0,
                             'subtitle' => null
-                        ])?>
+                        ]) ?>
 
                         <?= view('components/cards/card_stats', [
                             'title' => 'Total Orders Today:',
                             'value' => 0,
                             'subtitle' => null
-                        ])?>
+                        ]) ?>
 
                         <?= view('components/cards/card_stats', [
                             'title' => 'Total Orders This Week:',
                             'value' => 0,
                             'subtitle' => null
-                        ])?>
+                        ]) ?>
 
                         <?= view('components/cards/card_stats', [
                             'title' => 'Total Orders This Month:',
                             'value' => 0,
                             'subtitle' => null
-                        ])?>
+                        ]) ?>
                     </div>
                 </section>
 
@@ -52,8 +52,8 @@
                 <section>
                     <div class="gap-8 grid grid-cols-1 md:grid-cols-3">
                         <!-- Today -->
-                        <div class="p-4 color-light-cappuccino rounded-xl shadow">
-                            <h3 class="text-xl text-center text-color-dark-espresso font-semibold mb-4">
+                        <div class="shadow p-4 rounded-xl color-light-cappuccino">
+                            <h3 class="mb-4 font-semibold text-color-dark-espresso text-xl text-center">
                                 Most Sold Item Today
                             </h3>
                             <?= view('components/cards/card', [
@@ -61,12 +61,12 @@
                                 'description' => 'Smooth espresso blended with creamy milk',
                                 'price' => '₱150',
                                 'image' => 'https://upload.wikimedia.org/wikipedia/commons/d/d8/Caffe_Latte_at_Pulse_Cafe.jpg'
-                            ])?>
+                            ]) ?>
                         </div>
 
                         <!-- This Week -->
-                        <div class="p-4 color-light-cappuccino rounded-xl shadow">
-                            <h3 class="text-xl text-center text-color-dark-espresso font-semibold mb-4">
+                        <div class="shadow p-4 rounded-xl color-light-cappuccino">
+                            <h3 class="mb-4 font-semibold text-color-dark-espresso text-xl text-center">
                                 Most Sold Item This Week
                             </h3>
                             <?= view('components/cards/card', [
@@ -74,12 +74,12 @@
                                 'description' => 'Strong and bold, perfect for coffee lovers',
                                 'price' => '₱120',
                                 'image' => 'https://www.thespruceeats.com/thmb/HJrjMfXdLGHbgMhnM0fMkDx9XPQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/what-is-espresso-765702-hero-03_cropped-ffbc0c7cf45a46ff846843040c8f370c.jpg'
-                            ])?>
+                            ]) ?>
                         </div>
 
                         <!-- This Month -->
-                        <div class="p-4 color-light-cappuccino rounded-xl shadow">
-                            <h3 class="text-xl text-center text-color-dark-espresso font-semibold mb-4">
+                        <div class="shadow p-4 rounded-xl color-light-cappuccino">
+                            <h3 class="mb-4 font-semibold text-color-dark-espresso text-xl text-center">
                                 Most Sold Item This Month
                             </h3>
                             <?= view('components/cards/card', [
@@ -87,7 +87,7 @@
                                 'description' => 'Perfect balance of deep espresso, steamed milk, and rich chocolate',
                                 'price' => '₱200',
                                 'image' => 'https://www.folgerscoffee.com/folgers/recipes/_Hero%20Images/Detail%20Pages/5598/image-thumb__5598__schema_image/MochaIced-hero.58f3878d.jpg'
-                            ])?>
+                            ]) ?>
                         </div>
                     </div>
                 </section>
@@ -98,4 +98,5 @@
     <!-- Footer -->
     <?= view('components/footer') ?>
 </body>
+
 </html>
